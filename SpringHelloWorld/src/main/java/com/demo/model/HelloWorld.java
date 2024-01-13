2f1aea7f666b5f4aecbf80942b290cd08361fc1e
Yasir Shaikh <68939975+Yasir-Tec@users.noreply.github.com>
2024-01-13 03:32:47 -0800

    Spring Basics

---
+++ SpringHelloWorld/src/main/java/com/demo/model/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.demo.model;
+
+public class HelloWorld {
+	
+	public HelloWorld()
+	{
+		System.out.println("Hello world default constructor");
+	}
+	
+	public String Sayhello()
+	{
+		return "hii hellow";
+	}
+
+}
